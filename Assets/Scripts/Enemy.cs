7591bd459a039cb2170ab12e4703553c26abaf8a
Dima Romanenko <bitlgir17@gmail.com>
2025-07-24 01:52:03 +0300

    add laser debuff and code refactoring

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,47 +1,23 @@
 using UnityEngine;
 
+
 public class Enemy : MonoBehaviour
 {
+    public float startSpeed = 10f;
+
+    [HideInInspector]
     public float speed = 10f;
-    public int health = 100;
-    public int value = 50;
+
+    public float health = 100;
+    public int worth = 50;
 
     public GameObject deathEffect;
 
-    private Transform target;
-    private int wavePointIndex = 0;
-
     private void Start()
     {
-        target = WayPoints.points[0];
+        speed = startSpeed;
     }
-    private void Update()
-    {
-        Vector3 dir = target.position - transform.position;
-
-        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
-
-        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
-            GetNextWayPoint();
-    }
-    void GetNextWayPoint()
-    {
-        if (wavePointIndex >= WayPoints.points.Length - 1)
-        {            
-            EndPath();
-            return;
-        }
-
-        wavePointIndex++;
-        target = WayPoints.points[wavePointIndex];
-    }
-
-    void EndPath ()
-    {
-        PlayerStats.Lives--;
-        Destroy(gameObject);
-    }
-    public void TakeDamage(int amount)
+    public void TakeDamage(float amount)
     {
         health -= amount;
 
@@ -50,10 +26,15 @@ public class Enemy : MonoBehaviour
             Die();
         }
     }
+
+    public void Slow(float pct)
+    {
+        speed = startSpeed * (1 - pct);
+    }
     void Die()
     {
         GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
-        PlayerStats.Money += value;
+        PlayerStats.Money += worth;
         Destroy(gameObject);
         Destroy(effect, 5f);
     }
